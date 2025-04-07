026f2a7c2312fd7f0eee148678472f45e237fb70
hendrik <hendrik.berg@posteo.de>
2025-04-07 19:31:56 +0200

    fix ibus sync

--- src/receiver.rs
+++ src/receiver.rs
@@ -7,55 +7,45 @@ use parse_rc_ibus::{IbusPacket, ParsingError};
 use serialport;
 
 const MAX_ROLL: f32 = 30.0 / 180.0 * PI;
-const MAX_PITCH: f32 = 20.0 / 180.0;
+const MAX_PITCH: f32 = 20.0 / 180.0 * PI;
 const MAX_YAW_RATE: f32 = 90.0 / 180.0 * PI;
 
-const IBUS_HEADER: [u8; 2] = [0x20, 0x10];
+const IBUS_HEADER: [u8; 2] = [0x20, 0x40];
 
 pub fn handle_receiver(setpoint: Arc<Mutex<State>>) {
-    let mut port = serialport::new("/dev/ttyAMA2", 115_200)
+    let mut port = serialport::new("/dev/ttyACM0", 115_200)
         .timeout(Duration::from_millis(14))
         .open()
         .expect("Failed to open port");
 
     let mut buffer = [0u8; 32];
-    let mut header_buffer = [0u8; 2];
+    let mut header_buffer = [0u8; 1];
 
     loop {
         match port.read_exact(&mut buffer) {
             Ok(()) => {
-                if buffer[0..2] == IBUS_HEADER {
-                    match IbusPacket::try_from_bytes(&buffer) {
-                        Ok(packet) => {
-                            // get channels and map from -1 to 1
-                            let channels: [f32; 14] =
-                                packet.get_all_channels().map(|c| (c - 1500) as f32 / 500.0);
-                            *setpoint.lock().unwrap() = State {
-                                roll: channels[0] * MAX_ROLL,
-                                pitch: channels[1] * MAX_PITCH,
-                                yaw_rate: channels[4] * MAX_YAW_RATE,
-                                altitude: 30.0,
-                            }
+                match IbusPacket::try_from_bytes(&buffer) {
+                    Ok(packet) => {
+                        // get channels and map from -1 to 1
+                        let channels: [f32; 14] =
+                            packet.get_all_channels().map(|c| (c as f32 - 1500.0) / 500.0);
+                        *setpoint.lock().unwrap() = State {
+                            roll: channels[0] * MAX_ROLL,
+                            pitch: channels[1] * MAX_PITCH,
+                            yaw_rate: channels[3] * MAX_YAW_RATE,
+                            altitude: 30.0,
                         }
-                        Err(e) => match e {
-                            ParsingError::FailsChecksum => println!("invalid package"),
-                            ParsingError::InvalidPacket => {
-                                println!("desynced");
-                                loop {
-                                    // searching for header
-                                    let _ = port.read_exact(&mut header_buffer);
-                                    if header_buffer == IBUS_HEADER {
-                                        // drop the last 32 bytes to get back in sync
-                                        let _ = port.read_exact(&mut buffer[2..]);
-                                        break;
-                                    }
-                                }
-                            }
-                        },
                     }
+                    Err(e) => match e {
+                        ParsingError::FailsChecksum => println!("invalid package"),
+                        ParsingError::InvalidPacket => {
+                            println!("ibus desync!");
+                            let _ = port.read(&mut header_buffer);
+                        }
+                    },
                 }
             }
-            Err(e) => println!("{:?}",e),
+            Err(e) => println!("Reveiver Error {:?}", e),
         }
     }
 }
